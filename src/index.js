c63aa392da358b87aed32f2ca8756de8b6c73c72
Abdulrehman Butt <buttrehman594@gmail.com>
2025-04-13 11:47:03 +0500

    Connect to Mongo Atlas

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// 1st approach is Database connection in one module with express app initilized in it aswell!
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants"
+
+// import express from "express"
+// const app = express()
+
+// ;( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("Error:", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`)
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR:", error)
+//         throw err
+//     }
+// })()
+
+
+// 2nd
+
+
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
