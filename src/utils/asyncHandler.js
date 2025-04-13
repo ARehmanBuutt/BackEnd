e65013cc024f6293e0d21189496882d2d0df1583
Abdulrehman Butt <buttrehman594@gmail.com>
2025-04-13 12:58:35 +0500

    Utils Code Setup

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const aysncHandler =  (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+export { aysncHandler }
+
+
+// Database communication wrapper with Try Catch
+
+// const aysncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+// export { aysncHandler }
